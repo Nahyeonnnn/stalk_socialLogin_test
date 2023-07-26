cda376577e937f580814164c497549fc52c753c1
sunjae <zxsunje775@naver.com>
2023-07-26 16:13:43 +0900

    배경색,환영합니다,차트박스

--- src/component/main/mainGraph.jsx
+++ src/component/main/mainGraph.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+
+const ChartBox = styled.div`
+  display: flex;
+  width: 20rem;
+  height: 15rem;
+  background-color: rgba(241, 208, 10, 0.92);
+  border-radius: 1rem;
+  margin: auto;
+`;
+
+const ChartType = styled.p`
+  color: white;
+  font-size: small;
+  margin-left: 1rem;
+  font-weight: bold;
+`;
+
+const ChartButtonBox = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const ChartButton = styled.button`
+  background-color: lightgray;
+  width: 5px;
+  height: 12px;
+  border-radius: 50%;
+  border-style: none;
+  margin: 10px 5px;
+`;
 
 const MainGraph = () => {
-    return (
-        <></>
-    );
+  return (
+    <>
+      <ChartBox>
+        <ChartType>
+          코스피 <br></br> 2,609.76
+        </ChartType>
+      </ChartBox>
+      <ChartButtonBox>
+        <ChartButton></ChartButton>
+        <ChartButton></ChartButton>
+        <ChartButton></ChartButton>
+        <ChartButton></ChartButton>
+        <ChartButton></ChartButton>
+        <ChartButton></ChartButton>
+      </ChartButtonBox>
+    </>
+  );
 };
 
 export default MainGraph;
--- src/page/mainPage.jsx
+++ src/page/mainPage.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import MainInfo from "../component/main/mainInfo";
+import MainGraph from "../component/main/mainGraph";
+import MainMyInvest from "../component/main/mainMyInvest";
+import MainNews from "../component/main/mainNews";
+import MainRank from "../component/main/mainRank";
 
 const MainPage = () => {
-    return (
-       <>
-       
-       </>
-    );
+  return (
+    <>
+      <MainInfo></MainInfo>
+      <MainGraph></MainGraph>
+      <MainMyInvest></MainMyInvest>
+      <MainNews></MainNews>
+      <MainRank></MainRank>
+    </>
+  );
 };
 
 export default MainPage;
