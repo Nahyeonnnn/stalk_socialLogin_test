ef39d12ec7b1c9682e52355f347ef18c0277f3d6
nahyeon <k92544199@gmail.com>
2023-07-27 20:43:53 +0900

    자동완성 추가, 최근 검색 기능 미완, css 미완

--- src/component/search/searchBar.jsx
+++ src/component/search/searchBar.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import {BiSearch} from "@react-icons/all-files/bi/BiSearch"
 import TopBar from '../global/topBar';
 import BottomBar from '../global/bottomBar';
+import SearchRecent from './searchRecent';
 
 //axios 연결 시 받을 주식 리스트 예시
 //일단은 주식 이름 string이 들어있다고 가정함
@@ -48,7 +49,6 @@ const SearchInput = styled.input`
 
 const AutoSearchContainer = styled.div`
   width: inherit;
-  background-color: none;
   position: absolute;
   border: none;
 `;
@@ -62,25 +62,72 @@ const AutoSearchData = styled.li`
   z-index: 4;
   letter-spacing: 2px;
   &:hover {
-    background-color: #edf5f5;
     cursor: pointer;
-  }
-  position: relative;
-  img {
-    position: absolute;
-    right: 5px;
-    width: 18px;
-    top: 50%;
-    transform: translateY(-50%);
   }
 `;
 
 const SearchBar = () => {
 
+    //axios 연결 시 주식 리스트를 저장할 변수
+    // const [stockList, setStockList]=useState(testList);
+    //검색어를 저장하기 위한 useState
+    const [query, setQuery]=useState("");
+    const [suggestions, setSuggestions] = useState([]);
+    const [showSuggestions, setShowSuggestions]=useState(false);
+    const [searchResults, setSearchResults]=useState([]);//검색 결과 저장을 위한 useState
+
     function handleInputSearch(e){
-        setQuery(e.target.value);
-        console.log(query);
+        const {value} = e.target;
+        setQuery(value);
+        const SearchSuggestions = getSuggestions(value);
+        setSuggestions(SearchSuggestions);
+        setShowSuggestions(true);
     }
+
+    const getSuggestions = (value) => {
+        //검색어 자동 완성 결과를 가져오는 로직을 구현합니다.
+        //예시: 데이터는 배열 형태로 가정하고, 배열 요소 중에 입력된 검색어를 포함하는 항목들을 추출
+
+        const filteredSuggestions = stockList.filter((item)=>
+            item.prdt_name.includes(value)
+        );
+        return filteredSuggestions;
+    }
+
+    //중간 검색 과정에서의 키워드를 저장하는 함수
+    const handleSelectSuggestion = (suggestion) => {
+        setQuery(suggestion.prdt_name);
+        setSuggestions([]);
+        setShowSuggestions(false);
+    }
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        const results = searchData(query);
+        setSearchResults(results);
+        setSuggestions([]); // 자동완성 결과 초기화
+        setShowSuggestions(false);
+        if (typeof onSearch === "function") {
+          onSearch(results);
+        }
+    };
+
+    const searchData = (query) => {
+        const results = stockList.filter((item)=>
+            item.prdt_name.includes(query)
+        );
+        return results;
+    }
+
+    // const handleKeyPress = (e) => {
+    //     if(e.key == "Enter"){
+    //         handleFormSubmit(e);
+    //     }
+    // };
+
+    const handleInputBlur = () => {
+        setShowSuggestions(false);
+    };
 
     function SearchIconClick(){
         axios
@@ -93,10 +140,7 @@ const SearchBar = () => {
             });
     }
 
-    //axios 연결 시 주식 리스트를 저장할 변수
-    const [stockList, setStockList]=useState(testList);
-    //검색어를 저장하기 위한 useState
-    const [query, setQuery]=useState("");
+    //useEffect(()=>{},[]);
 
     //input 태그에 onchange, onblur, onkeypress 속성 추가
 
@@ -111,15 +155,34 @@ const SearchBar = () => {
                 <BiSearch onClick={SearchIconClick} color='gray'/>
                 </span>
                 <SearchInput type="text" value={query} onChange={handleInputSearch} placeholder='검색하기'></SearchInput>
+                {query.length > 0 && showSuggestions && (
                 <AutoSearchContainer>
                     <ul>
                         {
-                            stockList.map((result)=>(
-                                <AutoSearchData>{result.prdt_name}</AutoSearchData>
+                            suggestions.map((result)=>(
+                                <div>
+                                    <AutoSearchData>{result.prdt_name}</AutoSearchData>
+                                </div>
                             ))
                         }
                     </ul>
-                </AutoSearchContainer>
+                </AutoSearchContainer>)}
+                {searchResults.length > 0 && (
+                    <AutoSearchContainer>
+                        <ul>
+                            {
+                                searchResults.map((result)=>(
+                                    <div>
+                                        <AutoSearchData>{result.prdt_name}</AutoSearchData>
+                                    </div>
+                                ))
+                            }
+                        </ul>
+                    </AutoSearchContainer>
+                )
+
+                }
+                
             </SearchSmallContainer>
         </SearchContainer>
         <BottomBar></BottomBar>
