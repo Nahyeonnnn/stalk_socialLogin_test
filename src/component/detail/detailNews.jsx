2d386821712a322afb57b3b3f3c66b6d6713e5be
kim nahyeon <k92544199@gmail.com>
2023-08-06 01:35:23 +0900

    Update detailNews.jsx

--- src/component/detail/detailNews.jsx
+++ src/component/detail/detailNews.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { styled } from 'styled-components';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const NewsBox = styled.div`
     display : flex;
