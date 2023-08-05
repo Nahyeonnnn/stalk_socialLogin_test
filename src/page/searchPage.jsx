cebba158b5bf632ef2a5aa0964245fddf39ed30e
kim nahyeon <k92544199@gmail.com>
2023-08-06 01:42:16 +0900

    Update searchPage.jsx

--- src/page/searchPage.jsx
+++ src/page/searchPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SearchBar from '../component/search/searchBar';
-import SearchRecent from '../component/search/searchRecent';
+// import SearchRecent from '../component/search/searchRecent';
 import BottomBar from '../component/global/bottomBar';
 const SearchPage = () => {
     return (
