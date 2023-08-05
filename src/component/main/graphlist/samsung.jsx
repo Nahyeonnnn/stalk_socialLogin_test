4b0382e6e7b0e2e9f2639965caafbd0e5a134bde
kim nahyeon <k92544199@gmail.com>
2023-08-06 01:36:10 +0900

    Update samsung.jsx

--- src/component/main/graphlist/samsung.jsx
+++ src/component/main/graphlist/samsung.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import HighchartsReact from "highcharts-react-official";
 import Highcharts from "highcharts";
